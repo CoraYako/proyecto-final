f97a2c4c9302bd74fcb8edcc2c46c2a20b38d53f
Guillermo Pintos <pintosguillermoalberto@gmail.com>
2022-04-27 21:34:54 -0300

    agregado repositorio, servicio de entidad imagen. Modificacion de servicio espacioTarea

---
+++ src/main/java/com/proyectoFinal/exceptions/ElementoNoEncontradoException.java
@@ -0,0 +1,9 @@
+
+package com.proyectoFinal.exceptions;
+
+public class ElementoNoEncontradoException extends Exception {
+
+    public ElementoNoEncontradoException(String msn) {
+        super(msn);
+    }
+}
