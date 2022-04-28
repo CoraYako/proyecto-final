fb2de4dd2f6aa7f64e0703ff4bfe95f76bb8f7a4
matiasyerga <matiasyerga@gmail.com>
2022-04-27 22:27:57 -0300

    Refactor usuarioRepo y usuarioServicio.save()

--- src/main/java/com/proyectoFinal/repositorios/UsuarioRepository.java
+++ src/main/java/com/proyectoFinal/repositorios/UsuarioRepository.java
@@ -9,6 +9,23 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
 
+    //Buscar por Email
+    
     @Query("SELECT u FROM Usuario u WHERE u.email LIKE :email")
     public Usuario buscarPorEmail(@Param("email") String email);
+    
+    // Buscar por Rol
+    @Query("SELECT u FROM Usuario u WHERE u.rol like :rol")
+    public Usuario buscarPorRol(@Param("rol") String rol);
+    
+    // Buscar por Nombre 
+    @Query("SELECT u FROM Usuario u WHERE u.rol like :rol")
+    public Usuario buscarPorNombre(@Param("nombre") String nombre);
+    
+    // Buscar por Apellido
+    @Query("SELECT u FROM Usuario u WHERE u.rol like :rol")
+    public Usuario buscarPorApellido(@Param("Apellido") String apellido);
+    
+    
+    
 }
--- src/main/java/com/proyectoFinal/servicios/UsuarioService.java
+++ src/main/java/com/proyectoFinal/servicios/UsuarioService.java
@@ -4,6 +4,7 @@ import com.proyectoFinal.entidades.Usuario;
 import com.proyectoFinal.enums.Rol;
 import com.proyectoFinal.repositorios.UsuarioRepository;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
@@ -22,17 +23,39 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Service
 public class UsuarioService implements UserDetailsService {
 
+    
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    
     @Transactional(rollbackOn = { Exception.class })
-    public void save(String email, String password, String confirmarPassword) {
-        Usuario usuario = new Usuario();
-        usuario.setEmail(email);
+    public void save(String email, String password, String confirmarPassword,Rol rol, String nombre, String apellido, String fechaNacimiento,/*Imagen fotoPerfil,*/ Date fechaAlta, Date fechaBaja, Boolean activo  ) {
+        
+       //metodo para validar que el rol no sea nulo
+        if(password.equals(confirmarPassword)){
+        
+           Usuario u = new Usuario();
+        u.setEmail(email);
         String passwordEncriptado = new BCryptPasswordEncoder().encode(password);
-        usuario.setPassword(passwordEncriptado);
-        usuario.setRol(Rol.ADMIN);
-        usuarioRepository.save(usuario);
+        u.setPassword(passwordEncriptado);
+        u.setFechaNacimiento(fechaAlta);
+        u.getApellido();
+        u.setFechaAlta(fechaAlta);
+        u.setFechaBaja(fechaBaja);
+        u.setRol(rol);
+        //u.setImagen(imgPerfil);
+        u.setActivo(activo);
+     
+
+        //usuario.setRol(Rol.ADMIN);
+        usuarioRepository.save(u);
+        
+        }else{
+        
+            System.out.println(" Las contraseñas no coinciden");
+        
+        }
+        
     }
 
     public List<Usuario> getAll() {
