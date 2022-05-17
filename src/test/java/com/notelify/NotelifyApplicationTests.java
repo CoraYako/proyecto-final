5cdc6d9c7642f650ca6906e7071e2eaae26bf1ae
Hector Cortez <updown.input@gmail.com>
2022-05-17 19:19:54 -0300

    Cambios en el index, login y registro.

--- src/test/java/com/notelify/NotelifyApplicationTests.java
+++ src/test/java/com/notelify/NotelifyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.notlify;
+package com.notelify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotlifyApplicationTests {
+class NotelifyApplicationTests {
 
 	@Test
 	void contextLoads() {
