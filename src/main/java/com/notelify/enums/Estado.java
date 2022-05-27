a931f96c9cdacf80cd31bc0590743c38d3dd58f1
Hector Cortez <updown.input@gmail.com>
2022-05-26 22:41:03 -0300

    Fix al elegir una tarea de la lista 'Por Hacer'

--- src/main/java/com/notelify/enums/Estado.java
+++ src/main/java/com/notelify/enums/Estado.java
@@ -4,7 +4,7 @@ package com.notelify.enums;
     Definición de estados por los que pasarán las tareas.
 **/
 public enum Estado {
-    TODO("Para hacer"), IN_PROGRESS("En progreso"), FINISHED("Finalizadas"), PAUSED("En pausa");
+    TODO("Para hacer"), IN_PROGRESS("En progreso"), FINISHED("Finalizada"), PAUSED("En pausa");
 
     private final String valor;
 
